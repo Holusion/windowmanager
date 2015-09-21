4ea3ba1771cca570478a9b104d81d645b05f16de
Sebastien Dumetz <s.dumetz@holusion.com>
2015-09-21 18:00:37 +0200

    replaced dbus with dbus-native in interface creation

--- test/test_menuInterface_getDbus.js
+++ test/test_menuInterface_getDbus.js
@@ -1,32 +1,66 @@
 var MenuInterface = require("../lib/MenuInterface")
-, Promise = require('es6-promise').Promise;
+  , Promise = require('es6-promise').Promise
+  , dbus = require('dbus-native');
 
 describe("MenuInterface.getDbusInterface",function(){
-  beforeEach(function(){
-    var mint = new MenuInterface();
-    mint.bus = "org.foo.bar";
-    mint.path = "/org/foo/bar";
-    mint.panelInterface = "org.foo.bar.iface";
-    this.mint = mint;
-  })
-  it("init",function(done){
-    this.mint.getDbusInterface = function(cb){
-      cb(null,{fake:true});
-    }
-    this.mint.getCliInterface = function(cb){
-      expect(true).to.be.false; //Should not be called!
-    }
-    this.mint.init();
-    expect(this.mint.iface).to.be.instanceof(Promise);
-    this.mint.iface.catch(function(err){expect(err).to.be.null}).then(function(iface){
-      done();
-    })
-
-  })
-  it("can fail",function(done){
-    this.mint.getDbusInterface(function(err,iface){
-      expect(err).to.be.not.null;
-      done();
+  describe("mock",function(){
+    beforeEach(function(){
+      var mint = new MenuInterface();
+      mint.bus = "org.foo.bar";
+      mint.path = "/org/foo/bar";
+      mint.interface = "org.foo.bar.iface";
+      this.mint = mint;
+    });
+    it("init",function(done){
+      this.mint.getDbusInterface = function(cb){
+        cb(null,{fake:true});
+      }
+      this.mint.getCliInterface = function(cb){
+        expect(true).to.be.false; //Should not be called!
+      }
+      this.mint.init();
+      expect(this.mint.iface).to.be.instanceof(Promise);
+      this.mint.iface.catch(function(err){expect(err).to.be.null}).then(function(iface){
+        done();
+      });
+    });
+    it("detect undeclared service",function(done){
+      this.mint.getDbusInterface(function(err,iface){
+        expect(err).to.be.not.null;
+        expect(typeof iface).to.equal("undefined");
+        done();
+      });
+    });
+  });
+  describe("real",function(){
+    var mint, bus;
+    const name = "org.foo.bar",ipath="/org/foo/bar",iface="org.foo.bar"
+    before(function(){
+      bus = dbus.sessionBus();
+      bus.requestName(name, 0);
+      var exampleIface = {
+        name: iface,
+        methods: {
+            doStuff: ['s', 's']
+        }
+      };
+      var example = {
+        doStuff: function(s) {
+            return 'Received "' + s + '" - this is a reply';
+        }
+      };
+      bus.exportInterface(example, ipath, exampleIface);
+      mint = new MenuInterface();
+      mint.bus = name;
+      mint.path = ipath;
+      mint.interface = iface;
+    });
+    it("detect existing service and interface",function(done){
+      mint.getDbusInterface(function(err,iface){
+        expect(err).to.be.null;
+        expect(typeof iface).to.equal("object");
+        done();
+      });
     });
   });
 })
