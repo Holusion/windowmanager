de10fa13f3cfe1b32aa9db2a9e5ad6d562742d5d
Sebastien Dumetz <s.dumetz@holusion.com>
2020-06-04 11:53:58 +0200

    fix XErrorWindow.destroy(). add simple tests to XErrorWindow

---
+++ test/test_XErrorWindow.js
@@ -0,0 +1,42 @@
+'use strict';
+const x11 = require('x11');
+
+const {ErrorWindow} = require("../lib/Xutils/XErrorWindow");
+
+describe("XErrorWindow", ()=>{
+  let display, X, root, wid;
+  before((done)=>{
+    const client = x11.createClient(function(err,d){
+      expect(err).to.be.not.ok;
+      display = d;
+      X = d.client;
+      wid = X.AllocID();
+      X.CreateWindow(wid, display.screen[0].root, 0, 0, 1, 1, 0, 0, 0, 0, 
+        { eventMask: x11.eventMask.Exposure|x11.eventMask.SubstructureRedirect }
+      );
+      done(err);
+    });
+    client.on("error", (e)=>expect.fail(e));
+  })
+  after((done)=>{
+    X.DestroyWindow(wid);
+    X.ReleaseID(wid);
+    X.on("end", done);
+    X.terminate();
+  })
+  it("can destroy window", function(){
+    const e = new ErrorWindow({display, parent: root});
+    expect(()=>e.destroy()).not.to.throw(Error);
+  });
+
+  it("calling draw after destroy will throw an error", function(){
+    const e = new ErrorWindow({display, parent: root});
+    expect(()=>e.destroy()).not.to.throw(Error);
+    expect(()=>e.draw()).to.throw(Error);
+  });
+
+  it("can draw text", function(){
+    const e = new ErrorWindow({display, parent: root});
+    expect(()=>e.draw("Hello", "hello nodejs")).not.to.throw();
+  });
+})
